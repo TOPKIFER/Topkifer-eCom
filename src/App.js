9d469245a53ae59c985bacc5d08a0015a947f9de
RobertTk1 <rtopkiffeur1@gmail.com>
2020-08-13 14:08:47 -0500

    Cleaned up unecesary code, ready to start

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h2>WELCOME</h2>
-      <h3>TO</h3>
-      <h2>
-        Top<span>Kiffer</span>.com
-      </h2>
-      <p>Coming soon</p>
+      <h1>Hello World</h1>
     </div>
   );
 }
