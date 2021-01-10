cab6b4ee1898d26cd283281f614bf85edc1b7a2d
klm <58208740+klm-lab@users.noreply.github.com>
2021-01-10 21:00:33 +0000

    TK-8: set shop bag badge to 9+ instead of 99+

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,5 +1,5 @@
 export const multipleClasses = (...classes) => classes.join(" ");
-export const overHundred = (value) => value > 9 ? "9+" : value;
+export const overTen = (value) => value > 9 ? "9+" : value;
 export const toggleAuthVisibility = (setVisible, value) => {
     setVisible("isClosing", !value);
     setTimeout(() => {
